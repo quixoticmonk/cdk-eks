5890f13992edb4224af211d2c0f14070b59946c6
Manu Chandrasekhar <manuchandrasekhar@gmail.com>
2021-04-21 20:46:58 -0400

    eks cluster init

--- eks/eks_stack.py
+++ eks/eks_stack.py
@@ -1,15 +1,28 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (core as cdk, aws_eks as _eks, aws_ec2 as _ec2, aws_kms as
+                     _kms)
 
 
 class EksStack(cdk.Stack):
-
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str,
+                 **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        _secrets_key = _kms.Key(
+            self,
+            "clusterkey",
+            enabled=True,
+            enable_key_rotation=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            description="Key created for envelope encryption")
+
+        _cluster = _eks.Cluster(
+            self,
+            "cluster",
+            cluster_name="eks-cluster",
+            version=_eks.KubernetesVersion.V1_19,
+            default_capacity=5,
+            default_capacity_instance=_ec2.InstanceType.of(
+                _ec2.InstanceClass.MEMORY5, _ec2.InstanceSize.LARGE),
+            output_cluster_name=True,
+            output_config_command=True,
+        )
